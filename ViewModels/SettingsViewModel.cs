5546cdf55843ee4455417c2e0148618069404796
irydmy13 <kotiukir@gmail.com>
2025-10-16 16:54:03 +0300

    добавление языков en et ru

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -14,29 +14,64 @@ public class SettingsViewModel : INotifyPropertyChanged
     private readonly ISettingsService _settings;
     private readonly IAudioService _audio;
 
-    public string SelectedLanguage
+    public SettingsViewModel(
+        IThemeService theme,
+        ILocalizationService loc,
+        ISettingsService settings,
+        IAudioService audio)
     {
-        get => _settings.Language;
-        set { if (_settings.Language != value) { _settings.Language = value; _loc.SetCulture(value); OnPropertyChanged(nameof(SelectedLanguage)); } }
-    }
+        _theme = theme;
+        _loc = loc;
+        _settings = settings;
+        _audio = audio;
 
-    public bool SoundEnabled
-    {
-        get => _settings.SoundEnabled;
-        set { if (_settings.SoundEnabled != value) { _settings.SoundEnabled = value; _audio.IsEnabled = value; OnPropertyChanged(nameof(SoundEnabled)); } }
-    }
-
-    public IEnumerable<string> Languages => new[] { "en", "et" };
-
-    public ICommand SetLightCommand { get; }
-    public ICommand SetDarkCommand { get; }
-
-    public SettingsViewModel(IThemeService theme, ILocalizationService loc, ISettingsService settings, IAudioService audio)
-    {
-        _theme = theme; _loc = loc; _settings = settings; _audio = audio;
         _audio.IsEnabled = settings.SoundEnabled;
 
         SetLightCommand = new Command(() => _theme.Apply(AppTheme.Light));
         SetDarkCommand = new Command(() => _theme.Apply(AppTheme.Dark));
+        SetLanguageCommand = new Command<string>(lang => SelectedLanguage = lang);
     }
+
+    // --- ЯЗЫК ---
+    public string SelectedLanguage
+    {
+        get => _settings.Language;        
+        set
+        {
+            if (_settings.Language != value)
+            {
+                _settings.Language = value;
+                _loc.SetCulture(value);
+                OnPropertyChanged(nameof(SelectedLanguage));
+                OnPropertyChanged(nameof(IsEn));
+                OnPropertyChanged(nameof(IsEt));
+                OnPropertyChanged(nameof(IsRu));
+            }
+        }
+    }
+
+    // для подсветки выбранного флага
+    public bool IsEn => SelectedLanguage == "en";
+    public bool IsEt => SelectedLanguage == "et";
+    public bool IsRu => SelectedLanguage == "ru";
+
+    // --- ЗВУК ---
+    public bool SoundEnabled
+    {
+        get => _settings.SoundEnabled;
+        set
+        {
+            if (_settings.SoundEnabled != value)
+            {
+                _settings.SoundEnabled = value;
+                _audio.IsEnabled = value;
+                OnPropertyChanged(nameof(SoundEnabled));
+            }
+        }
+    }
+
+    // --- Команды ---
+    public ICommand SetLightCommand { get; }
+    public ICommand SetDarkCommand { get; }
+    public ICommand SetLanguageCommand { get; }
 }
